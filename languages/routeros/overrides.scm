35fe5cdcacae74e463bcb946070f6e66d756bbf9
Azagtot <3379450+Azagtot@users.noreply.github.com>
2026-04-13 13:16:08 +0300

    Publish RouterOS grammar and highlight fixes

--- languages/routeros/overrides.scm
+++ languages/routeros/overrides.scm
@@ -1,3 +1,13 @@
-[
-  (string)
-] @string
+(string) @attribute
+
+(parameter
+  value: (value
+    (string) @attribute))
+
+(parameter
+  value: (value
+    (variable) @variable))
+
+(parameter
+  value: (value
+    (word) @type))
